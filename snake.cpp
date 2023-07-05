63972193f2655470461cb47afb8010b03abc3a6b
usiop <jinwook.jun5@gmail.com>
2023-07-05 19:06:45 +0900

    dividing main cpp file

--- snake.cpp
+++ snake.cpp
@@ -1,4 +1,25 @@
 #include<iostream>
+using namespace std;
 
-int main() {
-}
+class block{
+private:
+    int size = 1;
+    int location_x = 0;
+    int location_y = 0;
+public:
+    void increase_size(){
+        this->size++;
+    }
+    void update_location_x(int x){
+        this->location_x += x;
+    }
+    void update_location_y(int y){
+        this->location_y += y;
+    }
+    void print_location_x(){
+
+    }
+    void print_location_y(){
+
+    }
+};
